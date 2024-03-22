320a5914d16bc83bd3c112a52a5781c60931351c
elgrandebashky <150351311+elgrandebashky@users.noreply.github.com>
2024-03-22 10:53:29 +0100

    Update hcrsro4.py

--- rpi/hcrsro4.py
+++ rpi/hcrsro4.py
@@ -3,12 +3,20 @@ import Adafruit_SSD1306
 from PIL import Image, ImageDraw, ImageFont
 from gpiozero import DistanceSensor
 import datetime
+import RPi.GPIO as GPIO
+from lib_oled96 import ssd1306
+from smbus import SMBus
 
-ultrasonic = DistanceSensor(echo=23, trigger=24)
+# GPIO setup
+led_pin = 7
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(led_pin, GPIO.OUT)
 
 # I2C bus configuration
 I2C_BUS_NUMBER = 1  # Use the correct bus number obtained from i2cdetect
 I2C_ADDRESS = 0x3C  # Address of the OLED display
+i2cbus = SMBus(1)  # 1 = Raspberry Pi but NOT early REV1 board
+oled = ssd1306(i2cbus)  # create oled object, nominating the correct I2C bus, default address
 
 # Initialize display
 disp = Adafruit_SSD1306.SSD1306_128_64(rst=None, i2c_bus=I2C_BUS_NUMBER, i2c_address=I2C_ADDRESS)
@@ -23,6 +31,9 @@ image = Image.new("1", (disp.width, disp.height))
 font = ImageFont.load_default()
 draw = ImageDraw.Draw(image)
 
+# Initialize ultrasonic sensor
+ultrasonic = DistanceSensor(echo=23, trigger=24)
+
 # Function to turn off the display completely
 def turn_off_display():
     # Clear the image buffer
@@ -31,6 +42,12 @@ def turn_off_display():
     disp.image(image)
     disp.display()
 
+def blink():
+    GPIO.output(led_pin, GPIO.HIGH)
+    print("on")
+    sleep(3)
+    GPIO.output(led_pin, GPIO.LOW)
+    print("off")
 
 while True:
     # Clear previous text
@@ -54,8 +71,17 @@ while True:
     disp.display()
     
     # Check if distance is above 5 cm and turn off the display if so
-    if distance_cm > 7:
+    if distance_cm < 5:
+        blink()
+        # put border around the screen:
+        oled.canvas.rectangle((0, 0, oled.width-1, oled.height-1), outline=1, fill=0)
+        led_status = "on" if GPIO.input(led_pin) == GPIO.HIGH else "off"
+        oled.canvas.text((40, 15), f'LED is {led_status}', fill=1)
+        oled.canvas.text((40, 40), f'Distance: {distance_cm:.2f} cm', fill=1)
+        # now display that canvas out to the hardware
+        oled.display()
+    else:
         turn_off_display()
-    
+
     # Wait for a short time before updating again
     time.sleep(1)
