3593dbbe5b71184daed6dff3233de9799d125324
Phong Le-Tran <144049752+phonkymail@users.noreply.github.com>
2024-03-24 16:00:17 +0100

    commit changes: git commit --amend

--- azure/mqtt/subscribe.py
+++
@@ -1,9 +0,0 @@
-import paho.mqtt.subscribe as subscribe
-print("subscribe mqtt active")
-def on_message_print(client, userdata, message):
-    print("%s %s" % (message.topic, message.payload))
-    if userdata["message_count"] >= 5:
-        # it's possible to stop the program by disconnecting
-        client.disconnect()
-
-subscribe.callback(on_message_print, "paho/test/topic", hostname="74.234.16.173", userdata={"message_count": 0})
